ffb3d756e202dc56e14a6d8b3c4044425ffb7410
Sanganu <kppkmsanu@gmail.com>
2023-04-23 16:43:52 -0600

    Removedheight for wierd alignment

--- client/src/components/Booklist.js
+++ client/src/components/Booklist.js
@@ -1,26 +1,26 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ADD_BOOK } from "../utils/mutation";
 import Auth from "../utils/auth";
-import {savedBooksList,getSavedBooksList} from "../utils/localStorage"
+import { savedBooksList, getSavedBooksList } from "../utils/localStorage"
 
 
 
 const BookList = ({ title, authors, id, description, image }) => {
-  const [savedBooks,setSavedBooks] = useState(getSavedBooksList())
+  const [savedBooks, setSavedBooks] = useState(getSavedBooksList())
   const [addBook, { error }] = useMutation(ADD_BOOK);
   const [status, setStatus] = useState("false")
   const saveBook = async () => {
     const userToken = Auth.loggedIn() ? Auth.getToken() : null;
-    if(!userToken){
+    if (!userToken) {
       return false;
     }
     try {
-      const {data} = await addBook({
+      const { data } = await addBook({
         variables: {
           bookApiData: {
             title: title,
-            authors: authors, 
+            authors: authors,
             id: id,
             description: description,
             image: image
@@ -28,47 +28,30 @@ const BookList = ({ title, authors, id, description, image }) => {
         }
       })
 
-      console.log("Book Saved",data)
-      setSavedBooks([...savedBooks,data._id])
+      console.log("Book Saved", data)
+      setSavedBooks([...savedBooks, data._id])
     } catch (err) {
       console.error("Error in saving book", err)
     }
   }
 
-  return (<div>
+  return (
 
-
-    <section>
-      <article>
-        <img src={image} size="medium"  />
-        <h4>{title}</h4>
-        <p>{description}</p>
-        <p>
-          <img name='user' />
-          Author/s:{authors}
-        </p>
-        {status ?
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={saveBook}  color='green'>
-            Save Book
-    </button> : <h6>Book already Saved</h6>}
+      <article className="card-project m-5 p-5">    
+           <h3 className="font-extrabold">Title: {title}</h3> 
+           <img src={image} alt={title} className="img-project" />
+           <h4 className="font-normal">Book Description: {description}</h4>
+           <p className="font-bold">Author/s:{authors}</p>
+                {status ?
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={saveBook} color='green'>
+                    Save Book
+                </button> : <h6>Book already Saved</h6>}
       </article>
-  
-    </section>
-  </div>)
-  
-  
-  }
-  
-// const BookList= ({searchTerm})=> {
-//     const{loading,error,data} = useQuery(GETNEWBOOKS,{variables:{searchTerm}});
-//     if(loading) {return "Please wait";}
-//     if (error) {return `Error ${error}`}
-//     if(data) {console.log(data);}
 
-//     // console.log(userData)
+    
+  )
 
-//     return(<>
-//     <h1>Book List</h1>
-//     </>)
-// }
+
+}
+
 export default BookList;
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -7,7 +7,6 @@ const Home = () => {
     const [searchBooks, setSearchBooks] = useState([])
     const [searchTerm, setSearchTerm] = useState("");
 
-
     const handleSearchBooks = (event) => {
         event.preventDefault()
         setSearchBooks(true)
@@ -16,49 +15,51 @@ const Home = () => {
             .then(response => {
 
                 let booksList = []
-                console.log(booksList)
+                console.log(response)
                 response.data.items.forEach(element => {
                     try {
                         booksList.push({
                             title: element.volumeInfo.title,
                             authors: element.volumeInfo.authors || ["Author information not available"],
-                            id: element.accessInfo.id,
+                            id: element.id,
                             description: element.volumeInfo.description,
-                            image: element.volumeInfo.imageLinks.smallThumbnail || "/images/placeholder-images.jpg"
+                            image: element.volumeInfo.imageLinks.smallThumbnail || element.volumeInfo.imageLinks.thumbnail
+                             || "/images/placeholder-images.jpg"
                         })
                     } catch (err) {
                         console.warn("unable to retrive", element, err)
                     }
                 });
                 setSearchBooks(booksList)
-                console.log(searchBooks, "API data")
+                console.log(searchBooks, "API data",booksList)
             })
 
     }
 
     return (<main className="container mx-auto ">
-        <section className="ui text container centered">
-            {searchBooks.length > 0 ? <>
-                {searchBooks.map((book, key) =>
+        <section className="container centered flex flex-wrap justify-evenly gap-x-2.5">
+            {searchBooks.length > 0 ?
+       
+                searchBooks.map((book, key) =>
                     <Booklist
-                        title={book.key} 
+                        title={book.title} 
                         authors={book.authors}
                         id={book.id}
                         description={book.description}
                         image={book.image}
-                        key={key} />)}
+                        key={key} />)
+                    
 
-            </>
-                :<div className="page-container ml-0 bg-white rounded-xl shadow-md overflow-hidden ">
+                :
+                <div className="page-container ml-0 bg-white rounded-xl shadow-md overflow-hidden ">
                 <form  className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                    <h3>Search Books</h3>
                     <input  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type='text' onChange={(event) => setSearchTerm(event.target.value)} value={searchTerm} placeholder="Enter Search String" />
                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"  
                     onClick={handleSearchBooks} >Search Books</button>
                 </form>
-                      </div>
+                </div>
             }
-      
         </section>
     </main>)
 }
